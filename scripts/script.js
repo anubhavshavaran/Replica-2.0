50ba05159ca16e7c6c6689932ff2f6decd00db56
Anubhav Shavaran <91604480+anubhavshavaran@users.noreply.github.com>
2023-10-03 23:42:06 +0530

    Add files via upload
    All files added.

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+// Sign in button.
+const signInButton = document.querySelector('#signin-btn');
+signInButton.addEventListener('click', () => {
+    alert('User signed in.');
+});
+
+// Start Button.
+const startButtons = document.querySelectorAll('.start-btn');
+for (let i = 0; i < startButtons.length; i++) {
+    startButtons[i].addEventListener('click', () => {
+        alert('Start button clicked.');
+    });
+}
+
+// Book Call Buttons.
+const bookCallButtons = document.querySelectorAll('.book-call');
+const callTitles = document.querySelectorAll('.path-title h3');
+
+for (let i = 0; i < bookCallButtons.length; i++) {
+    let alertMessage = '';
+
+    bookCallButtons[i].addEventListener('click', () => {
+        switch (i) {
+            case 0:
+                alertMessage = `You booked a call for ${callTitles[i].innerText}!`;
+                break;
+                
+            case 1:
+                alertMessage = `You booked a call for ${callTitles[i].innerText}!`;
+                break;
+                
+            case 2:
+                alertMessage = `You booked a call for ${callTitles[i].innerText}!`;                
+                break;
+        }
+        alert(alertMessage);
+    });
+}
